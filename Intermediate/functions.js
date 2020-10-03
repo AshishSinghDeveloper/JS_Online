4f3b11565b92e802527f4f7b320e8cc26b6b1027
AshishSinghDeveloper <ashishsng21@hotmail.com>
2020-10-03 15:46:37 -0500

    JS function intro

---
+++ Intermediate/functions.js
@@ -0,0 +1,9 @@
+function Hello(name) {
+    return `Hello ${name}, enjoy your day`;
+}
+console.log(Hello("Ashish"));
+
+var Hello = function(name) {
+    return `Hello ${name}, enjoy your day`;
+}
+console.log(Hello("Ashish"));
