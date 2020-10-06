22ea3c04b4cd0e860590e0a0ec5b6cd75c1a9484
AshishSinghDeveloper <ashishsng21@hotmail.com>
2020-10-06 16:59:35 -0500

    Adds callback function and self-invoking Anonlymous function

--- Intermediate/functions.js
+++ Intermediate/functions.js
@@ -7,3 +7,14 @@ var Hello = function(name) {
     return `Hello ${name}, enjoy your day`;
 }
 console.log(Hello("Ashish"));
+
+//callback function
+var HelloAgain = (name) => {
+    return `Hello ${name}, enjoy your day`;
+}
+console.log(HelloAgain("Ashish"));
+
+//Self-invoking anonymous function. Immediately Invoked Function Expresson (IIFE)
+(function() {
+    console.log(`Hello, enjoy your day`)
+})();
